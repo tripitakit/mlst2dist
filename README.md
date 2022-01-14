8316e9231746b72e4fcd9c65160a9570ec416ab7
patrick <patrick.demarta@gmail.com>
2022-01-14 10:39:59 +0100

    alpha

---
+++ README.md
@@ -0,0 +1,108 @@
+# mlst2dist
+
+This program converts a chewBBACA alleles table into a dissimilarity matrix.
+
+It substitutes all strings "LNF", "NIPH", "PLOT3", "PLOT5"  with 0 (zero, integer), converts all "INF-X" in the corresponding "X" inferred allele-calls, converts anything else that isn't an integer value into "0", then calculates a matrix of pairwise dissimilarities using the Hamming distances corrected for missing data for 
+
+The dissimilarity matrix output is saved to disk with default PHYLIP square matrix format, or optional TSV format.
+
+
+    usage: mlst2dist.py [-h] [-f {PHYLIP,TSV}] input output
+
+    Converts a chewBBACA alleles table into a dissimilarity matrix.
+
+    positional arguments:
+      input                 chewBBACA alleles table .tsv
+      output                distance matrix output filename
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -f {PHYLIP,TSV}, --outfmt {PHYLIP,TSV}
+                            format options for the output matrix, default to PHYLIP
+
+
+## Example
+
+### Input alleles table (test_allele.tsv)
+
+    FILE	G1	G2	G3	G4	G5	G6	G7	G8	G9
+    S1	1	INF-2	3	2	1	5	NIPH	ASM	ALM
+    S2	1	1	1	1	NIPH	5	1	NIPHEM	INF-1
+    S3	INF-1	2	3	4	1	3	INF-1	LOTSC	1
+    S4	1	LNF	2	4	1	3	1	LNF	2
+    S5	1	2	ASM	2	1	3	2	1	LNF
+    S6	2	INF-8	3	PLOT3	PLOT5	3	INF-2	INF-1	INF-1
+
+
+### Dissimilarity matrix (PHYLIP squared matrix format)
+
+    $ mlst2dist.py test_alleles.tsv test_dist.phy
+    $ cat test_dist.phy
+    6
+    S1        	0.0	0.66667	0.44444	0.66667	0.44444	0.88889
+    S2        	0.66667	0.0	0.55556	0.66667	0.88889	0.77778
+    S3        	0.44444	0.55556	0.0	0.33333	0.55556	0.66667
+    S4        	0.66667	0.66667	0.33333	0.0	0.66667	0.88889
+    S5        	0.44444	0.88889	0.55556	0.66667	0.0	0.66667
+    S6        	0.88889	0.77778	0.66667	0.88889	0.66667	0.0
+
+### Dissimilarity matrix (TSV format)
+
+    $ mlst2dist.py test_alleles.tsv test_dist.tsv --outfmt TSV
+    $ cat test_dist.tsv
+
+      S1	S2	S3	S4	S5	S6
+    S1	0.0	0.66667	0.44444	0.66667	0.44444	0.88889
+    S2	0.66667	0.0	0.55556	0.66667	0.88889	0.77778
+    S3	0.44444	0.55556	0.0	0.33333	0.55556	0.66667
+    S4	0.66667	0.66667	0.33333	0.0	0.66667	0.88889
+    S5	0.44444	0.88889	0.55556	0.66667	0.0	0.66667
+    S6	0.88889	0.77778	0.66667	0.88889	0.66667	0.0
+
+
+### PHYLIP NEIGHBOR - UPGMA outfile
+
+    $ cat outfile
+
+      6 Populations
+
+    Neighbor-Joining/UPGMA method version 3.697
+
+
+    UPGMA method
+
+    Negative branch lengths allowed
+
+
+                +------------S1        
+            +---2 
+            !   +------------S5        
+        +---3 
+        !   !      +---------S3        
+      +-4   +------1 
+      ! !          +---------S4        
+    --5 ! 
+      ! +--------------------S2        
+      ! 
+      +----------------------S6        
+
+
+    From     To            Length          Height
+    ----     --            ------          ------
+      5        4          0.04167         0.04167
+      4        3          0.05556         0.09722
+      3        2          0.06945         0.16667
+      2     S1            0.22222         0.38889
+      2     S5            0.22222         0.38889
+      3        1          0.12500         0.22222
+      1     S3            0.16667         0.38889
+      1     S4            0.16667         0.38889
+      4     S2            0.34722         0.38889
+      5     S6            0.38889         0.38889
+
+
+### PHYLIP NEIGHBOR - UPGMA outtree
+
+    $ cat outtree
+    ((((S1:0.22222,S5:0.22222):0.06945,(S3:0.16667,S4:0.16667):0.12500):0.05556,
+    S2:0.34722):0.04167,S6:0.38889);
---
+++ mlst2dist.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+
+"""
+mlst2dist.py
+This program converts a chewBBACA alleles table into a dissimilarity matrix.
+It substitutes all strings "LNF", "NIPH", "PLOT3", "PLOT5"  with 0 (zero, integer)
+and removes all "INF-" substrings in allele-calls, then calculates pairwise dissimilarities
+using the Hamming distances corrected for missing data for all the samples pairs.py
+The dissimilarity matrix output is saved to disk with default PHYLIP square matrix format.
+"""
+
+
+__author__ = "Patrick De Marta"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import argparse
+import os
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Converts a chewBBACA alleles table into a dissimilarity matrix.")
+
+    parser.add_argument("input", help="chewBBACA alleles table .tsv")
+    parser.add_argument("output", help="distance matrix output filename")
+    parser.add_argument("-f", "--outfmt", choices=["PHYLIP", "TSV", "CSV"], default="PHYLIP",
+                        help="format options for the output matrix, default to PHYLIP")
+    args = parser.parse_args()
+    return args
+
+
+def sanitize(line):
+    sanitized_line = line.replace("\n", "").split("\t")
+    for index, a in list(enumerate(sanitized_line))[1:]:
+        sanitized_allele = ""
+        if "INF-" in a:
+            sanitized_allele = a.replace("INF-", "")
+        elif a in ["LNF", "PLOT3", "PLOT5", "NIPH", "NIPHEM", "ASM", "ALM", "LOTSC"]:
+            sanitized_allele = "0"
+        else:
+            try:
+                sanitized_allele = str(int(a))
+            except:
+                print(f"Unexpected allele call: {a}")
+                sanitized_allele = "0"
+
+        sanitized_line[index] = sanitized_allele.replace(" ", "")
+    return(sanitized_line)
+
+
+def transform_alleles_table(input):
+    allele_matrix = []
+    loci_vector = []
+    samples_vector = []
+
+    with open(input, "r") as alleles:
+        for index, line in enumerate(alleles):
+            if (index != 0):
+                sanitized_line = sanitize(line)
+                samples_vector.append(sanitized_line[0])
+                allele_matrix.append(sanitized_line[1:])
+            else:
+                loci_vector = line.replace("\n", "").split("\t")[1:]
+    return(samples_vector, loci_vector, allele_matrix)
+
+
+def make_matrix(input, output, outfmt):
+    samples, loci, alleles_matrix = transform_alleles_table(
+        input)
+
+    print(samples)
+
+    nloci = len(loci)
+    nsamp = len(samples)
+
+    if outfmt == "PHYLIP":
+        header = f"{nsamp}\n"
+    if outfmt == "TSV":
+        header = "\t" + "\t".join(samples) + "\n"
+
+    matrix_output_body_text = ""
+    for samp_i in range(nsamp):
+        samp_a = samples[samp_i]
+        alleles_vector_a = alleles_matrix[samp_i]
+
+        if outfmt == "PHYLIP":
+            samp_line = f"{samp_a.ljust(10)}"
+        if outfmt == "TSV":
+            samp_line = f"{samp_a}"
+
+        for samp_j in range(nsamp):
+            samp_b = samples[samp_j]
+            print(f'Processing {samp_a} - {samp_b}')
+            alleles_vector_b = alleles_matrix[samp_j]
+
+            n_matches = 0
+            n_missing = 0
+            for x in range(nloci):
+                if (alleles_vector_a[x] == alleles_vector_b[x]):
+                    n_matches += 1
+                else:
+                    if (alleles_vector_a[x] == 0 or alleles_vector_b[x] == 0):
+                        n_missing += 1
+
+            corrected_hamming_dist_a_b = round(1 - (
+                n_matches/nloci) + (n_missing/(2*nloci)), 5)
+
+            samp_line += f"\t{corrected_hamming_dist_a_b}"
+        matrix_output_body_text += f"{samp_line}\n"
+
+    # write the output matrix file
+    outh = open(output, "w")
+    outh.write(header)
+    outh.write(matrix_output_body_text)
+    outh.close()
+
+
+def validate_infile(input):
+    return os.path.exists(input)
+
+
+def main():
+    inputs = parse_args()
+    input = inputs.input
+    output = inputs.output
+    outfmt = inputs.outfmt
+
+    if validate_infile(input):
+        make_matrix(input, output, outfmt)
+        print(f"Done. The dissimilarity matrix has been saved in {output}")
+
+    else:
+        raise RuntimeError('Failed to open the input file.')
+
+
+if __name__ == "__main__":
+    main()
